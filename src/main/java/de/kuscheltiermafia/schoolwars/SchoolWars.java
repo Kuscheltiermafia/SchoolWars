0373b574aac21c6b5a89039d3156b82cb0b8d23f
BuildTools <unconfigured@null.spigotmc.org>
2024-01-14 21:46:36 +0100

    bissl particle aba geht nd

--- src/main/java/de/kuscheltiermafia/schoolwars/SchoolWars.java
+++ src/main/java/de/kuscheltiermafia/schoolwars/SchoolWars.java
@@ -2,14 +2,20 @@ package de.kuscheltiermafia.schoolwars;
 
 import de.kuscheltiermafia.schoolwars.events.JoinEvent;
 import de.kuscheltiermafia.schoolwars.gameprep.Teams;
+import jdk.tools.jmod.Main;
 import org.bukkit.Bukkit;
+import org.bukkit.plugin.Plugin;
 import org.bukkit.plugin.PluginManager;
 import org.bukkit.plugin.java.JavaPlugin;
 
 public final class SchoolWars extends JavaPlugin {
 
+    public static SchoolWars plugin;
+
     @Override
     public void onEnable() {
+
+        plugin = this;
 
         Teams.prep();
 
@@ -23,4 +29,9 @@ public final class SchoolWars extends JavaPlugin {
     public void onDisable() {
         // Plugin shutdown logic
     }
+
+    public static SchoolWars getPlugin(){
+        return plugin;
+    }
+
 }
--- src/main/java/de/kuscheltiermafia/schoolwars/events/JoinEvent.java
+++ src/main/java/de/kuscheltiermafia/schoolwars/events/JoinEvent.java
@@ -1,5 +1,6 @@
 package de.kuscheltiermafia.schoolwars.events;
 
+import de.kuscheltiermafia.schoolwars.SchoolWars;
 import org.bukkit.Bukkit;
 import org.bukkit.ChatColor;
 import org.bukkit.Location;
@@ -8,6 +9,7 @@ import org.bukkit.entity.Player;
 import org.bukkit.event.EventHandler;
 import org.bukkit.event.Listener;
 import org.bukkit.event.player.PlayerJoinEvent;
+import org.bukkit.scheduler.BukkitRunnable;
 
 public class JoinEvent implements Listener {
 
@@ -19,8 +21,13 @@ public class JoinEvent implements Listener {
         p.teleport(new Location(p.getWorld(), -24, 80, 175));
 
         for (Player pl : Bukkit.getOnlinePlayers()){
-            pl.spawnParticle(Particle.SCULK_SOUL, p.getLocation(), 100, 0.1, 0.1, 0.1);
+
+            pl.spawnParticle(Particle.LAVA, p.getLocation(), 1, 0.01, 0.01, 0.01);
+//            pl.spawnParticle(Particle.EXPLOSION_LARGE, p.getLocation(), 5, 1);
+//            pl.spawnParticle(Particle.PORTAL, p.getLocation(), 200, 4);
+
         }
+
 
         p.sendMessage( ChatColor.YELLOW + "---------------SCHOOL WARS---------------");
         p.sendMessage("Willkommen in diesem grandiosen Spiel,");
