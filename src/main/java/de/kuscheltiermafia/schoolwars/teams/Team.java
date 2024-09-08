6453fcf41da85e1226ddca6c1cdfbd6e85d437ce
Morgon <moritz.olthoff@gmail.com>
2024-09-08 20:40:56 +0200

    hab vergessen

--- src/main/java/de/kuscheltiermafia/schoolwars/teams/Team.java
+++ src/main/java/de/kuscheltiermafia/schoolwars/teams/Team.java
@@ -12,26 +12,30 @@ import org.bukkit.potion.PotionEffectType;
 
 import java.util.ArrayList;
 
+import static de.kuscheltiermafia.schoolwars.SchoolWars.world;
 import static de.kuscheltiermafia.schoolwars.mechanics.Ranzen.ranzenAmount;
 
 public enum Team {
-    SPORTLER(ChatColor.DARK_RED + "sportler", ChatColor.DARK_RED + "[Sport] ", ChatColor.DARK_RED + "Sportler", Items.sport_ranzen),
-    SPRACHLER(ChatColor.GOLD + "sprachler", ChatColor.GOLD + "[Sprache] ", ChatColor.GOLD + "Sprachler", Items.sprach_ranzen),
-    NWS(ChatColor.GREEN + "Naturwissenschaftler", ChatColor.GREEN + "[NWS] ", ChatColor.GREEN + "Naturwissenschaftler", Items.nws_ranzen);
+    SPORTLER(ChatColor.DARK_RED + "sportler", ChatColor.DARK_RED + "[Sport] ", ChatColor.DARK_RED + "Sportler", Items.sport_ranzen, new Location(world, 68.0, 80.0, 167.0, -90, 0)),
+    SPRACHLER(ChatColor.GOLD + "sprachler", ChatColor.GOLD + "[Sprache] ", ChatColor.GOLD + "Sprachler", Items.sprach_ranzen, new Location(world, -21.0, 88.0, 146.0, -90, 0)),
+    NWS(ChatColor.GREEN + "Naturwissenschaftler", ChatColor.GREEN + "[NWS] ", ChatColor.GREEN + "Naturwissenschaftler", Items.nws_ranzen, new  Location(world, 4.5, 81.0, 191.5, -90, 0)),;
 
     public final String teamName;
     public final String prefix;
     public final String joinMessage;
     public final ItemStack ranzen;
+    public final Location spawn;
 
     public ArrayList<String> mitglieder;
     public static double sekiRisk;
 
-    Team(String teamName, String prefix, String joinMessage, ItemStack ranzen) {
+    Team(String teamName, String prefix, String joinMessage, ItemStack ranzen, Location spawn) {
         this.teamName = teamName;
         this.prefix = prefix;
         this.joinMessage = joinMessage;
         this.ranzen = ranzen;
+        this.spawn = spawn;
+
         mitglieder = new ArrayList<>();
     }
 
@@ -66,3 +70,4 @@ public enum Team {
     }
 
 }
+
